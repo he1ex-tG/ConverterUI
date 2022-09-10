7433977019bc63131f09879c7f75ba5ce5bc72ac
he1ex-tG <gosu.he1ex@gmail.com>
2022-09-10 22:10:01 +0500

    Initial commit

---
+++ ConverterUI/src/test/kotlin/com/he1extg/ConverterUI/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.he1extg.ConverterUI
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

94267b2358906ac38bdab2dd318de62824e5d445
he1ex-tG <gosu.he1ex@gmail.com>
2022-11-21 23:29:05 +0500

    index.html and base controller functions

--- src/main/kotlin/com/he1extg/converterui/controller/HttpController.kt
+++ src/main/kotlin/com/he1extg/converterui/controller/HttpController.kt
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.multipart.MultipartFile
 
 @Controller
 class HttpController {
@@ -14,6 +17,12 @@ class HttpController {
 
     @GetMapping
     fun main(model: Model): String {
+        return "index"
+    }
+
+    @PostMapping
+    fun upload(@RequestBody file: MultipartFile): String {
+        println(file.resource.filename)
         return "index"
     }
 }

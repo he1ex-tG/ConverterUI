75bb45056fea6ab16894bc55e28d87c7db091990
he1ex-tG <gosu.he1ex@gmail.com>
2022-11-22 23:36:55 +0500

    Model based main page building

---
+++ src/main/kotlin/com/he1extg/converterui/controller/ConvertFile.kt
@@ -0,0 +1,39 @@
+package com.he1extg.converterui.controller
+
+import com.he1extg.converterui.model.ConverterFile
+import com.he1extg.converterui.service.ConverterService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.SessionAttributes
+import org.springframework.web.bind.support.SessionStatus
+
+@Controller
+@SessionAttributes("converterFile")
+class ConvertFile {
+
+    @Autowired
+    lateinit var converterService: ConverterService
+
+    @ModelAttribute(name = "converterFile")
+    fun converterFile(): ConverterFile {
+        return ConverterFile()
+    }
+
+    @GetMapping
+    fun main(): String {
+        return "index"
+    }
+
+    @PostMapping
+    fun upload(
+        @ModelAttribute converterFile: ConverterFile,
+        sessionStatus: SessionStatus
+    ): String {
+        println(converterFile.file?.resource?.filename)
+        sessionStatus.setComplete()
+        return "index"
+    }
+}
---
+++ src/main/kotlin/com/he1extg/converterui/model/ConverterFile.kt
@@ -0,0 +1,7 @@
+package com.he1extg.converterui.model
+
+import org.springframework.web.multipart.MultipartFile
+
+class ConverterFile {
+    var file: MultipartFile? = null
+}

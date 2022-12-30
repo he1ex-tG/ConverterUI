9102f3e530f322892cae4fa0503973749a0b0c0b
he1ex-tG <gosu.he1ex@gmail.com>
2022-12-30 23:40:54 +0500

    Added controller tests

---
+++ src/test/kotlin/com/he1extg/converterui/controller/ConverterFileTest.kt
@@ -0,0 +1,34 @@
+package com.he1extg.converterui.controller
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.core.io.FileSystemResource
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.RequestEntity
+import org.springframework.util.LinkedMultiValueMap
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ConverterFileTest {
+
+    @Autowired
+    lateinit var testRestTemplate: TestRestTemplate
+
+    @Test
+    fun `upload correct file`() {
+        val requestEntity = RequestEntity.post("/")
+            .contentType(MediaType.MULTIPART_FORM_DATA)
+            .body(
+                LinkedMultiValueMap<String, Any>().apply {
+                    add("file", FileSystemResource("E:/test.pdf"))
+                }
+            )
+
+        val answer = testRestTemplate.exchange(requestEntity, String::class.java)
+
+        assertThat(answer.statusCode).isEqualTo(HttpStatus.OK)
+    }
+}

68870453dbc9cb85e4942b7b902cc4ac1ebe2950
he1ex-tG <gosu.he1ex@gmail.com>
2023-02-25 21:15:21 +0500

    Rework UserService

--- src/main/kotlin/com/he1extg/converterui/service/UserService.kt
+++ src/main/kotlin/com/he1extg/converterui/service/UserService.kt
@@ -1,11 +1,10 @@
 package com.he1extg.converterui.service
 
-import com.he1extg.converterui.dto.user.NewUserDTO
 import com.he1extg.converterui.dto.user.UserDTO
 
 interface UserService {
 
-    fun addUser(block: () -> NewUserDTO): UserDTO
+    fun addUser(username: String, password: String): UserDTO
 
     fun getUser(username: String): UserDTO
 }
--- src/main/kotlin/com/he1extg/converterui/service/UserServiceImpl.kt
+++ src/main/kotlin/com/he1extg/converterui/service/UserServiceImpl.kt
@@ -4,18 +4,18 @@ import com.he1extg.converterui.dto.user.NewUserDTO
 import com.he1extg.converterui.dto.user.UserDTO
 import com.he1extg.converterui.feign.DataClient
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
 
+@Service
 class UserServiceImpl(
     private val dataClient: DataClient,
     private val passwordEncoder: PasswordEncoder
 ) : UserService {
 
-    override fun addUser(block: () -> NewUserDTO): UserDTO {
-        return dataClient.addUser {
-            block().apply {
-                passwordEncoder.encode(password)
-            }
-        }
+    override fun addUser(username: String, password: String): UserDTO {
+        val encodedPassword = passwordEncoder.encode(password)
+        val newUserDTO = NewUserDTO(username, encodedPassword)
+        return dataClient.addUser(newUserDTO)
     }
 
     override fun getUser(username: String): UserDTO {
